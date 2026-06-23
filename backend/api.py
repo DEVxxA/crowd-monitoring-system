2233585cdfd0edd038e4ce4a1c63ff9e4e2513ed
DEVAAAAAAAAAAAAAAA <r1574jayakumar@gmail.com>
2026-06-24 01:22:24 +0530

    Fix FastAPI template rendering

--- backend/api.py
+++ backend/api.py
@@ -129,9 +129,10 @@ class DeleteContact(BaseModel):
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html"
     )
+    
 
 @app.get("/metrics")
 async def metrics():
